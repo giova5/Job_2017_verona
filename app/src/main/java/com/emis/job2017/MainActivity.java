c9dc0defe7fdbeef976ad2289778e10572472919
gioc <giovanni.cirillo5@gmail.com>
2017-10-17 11:41:01 +0200

    Initial commit

---
+++ app/src/main/java/com/emis/job2017/MainActivity.java
@@ -0,0 +1,13 @@
+package com.emis.job2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
